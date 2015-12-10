4fb680ad841086fe8b43dc3b002921764d74ac59
Moises Apaza <moisesapaza07@gmail.com>
2015-12-10 02:19:44 -0300

    personalization tabs

--- CustomTabs/gen/com/example/customtabs/R.java
+++ CustomTabs/gen/com/example/customtabs/R.java
@@ -22,16 +22,30 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int tab_bg_selected=0x7f020001;
+        public static final int tab_bg_selector=0x7f020002;
+        public static final int tab_bg_unselected=0x7f020003;
+        public static final int tab_focused=0x7f020004;
+        public static final int tab_indicator=0x7f020005;
+        public static final int tab_pressed=0x7f020006;
+        public static final int tab_selected=0x7f020007;
+        public static final int tab_text_selector=0x7f020008;
+        public static final int tab_unselected=0x7f020009;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f080000;
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080007;
+        public static final int imageView=0x7f080004;
         public static final int tab1=0x7f080002;
         public static final int tab2=0x7f080003;
         public static final int tabhost=0x7f080001;
+        public static final int tabsText=0x7f080006;
+        public static final int textView=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int tab_layout=0x7f030001;
+        public static final int tabs_bg=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- CustomTabs/src/com/example/customtabs/MainActivity.java
+++ CustomTabs/src/com/example/customtabs/MainActivity.java
@@ -2,20 +2,62 @@ package com.example.customtabs;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
 import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	public TabHost tabs;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
 		Resources res = getResources();
-		TabHost tabs = (TabHost)findViewById(R.id.tabhost);
+		tabs = (TabHost)findViewById(R.id.tabhost);
+		tabs.setup();
+		
+//		setupTab(new TextView(this), "TAB 1");
+//		setupTab(new TextView(this), "TAB 2");
+		
+		TabHost.TabSpec spec1 = tabs.newTabSpec("Tab 1");
+		spec1.setContent(R.id.tab1);
+		spec1.setIndicator("TAB 1");
+		tabs.addTab(spec1);
+		
+		spec1 = tabs.newTabSpec("Tab 2");
+		spec1.setContent(R.id.tab2);
+		spec1.setIndicator("TAB 2", res.getDrawable(R.drawable.ic_launcher));
+		tabs.addTab(spec1);
+		
+		tabs.setCurrentTab(0);
 	}
+	
+//	private void setupTab(final View view, final String tag){
+//		View tabview = createTabView(tabs.getContext(), tag);
+//	    TabSpec setContent = tabs.newTabSpec(tag).setIndicator(tabview).setContent(new TabContentFactory() {
+//
+//			public View createTabContent(String tag) {
+//				// TODO Auto-generated method stub
+//				return null;
+//			}
+//		});
+//	    tabs.addTab(setContent);
+//	}
+//	
+//	private static View createTabView(final Context context, final String text){
+//		 View view = LayoutInflater.from(context).inflate(R.layout.tabs_bg, null);
+//		    TextView tv = (TextView) view.findViewById(R.id.tabsText);
+//		    tv.setText(text);
+//		    return view;
+//	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
